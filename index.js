3fbb391d8cab3211768919f252c362f137b23af6
Andrew Leazer <andrewdleazer@gmail.com>
2022-03-21 20:44:07 -0400

    step 3

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.prompt('Do you head left or right?')
-let answer  = window.prompt('Do you head left or right?')
-console.log(firstAnswer)
+let firstAnswer =  window.prompt('Do you head left or right?')
+console.log(firstAnswer === 'left')
